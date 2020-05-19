df7563c8f4ed189a7af49f7dbd932d054ab05c8d
Chirag Thakkar <chirag.thakkar1993@gmail.com>
2020-05-19 05:35:04 +0530

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+$(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+    
+        var itemWidth = $(this).data('level');
+        
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+        
+    });
+
+});
+
+
+jQuery(document).ready(function($) {
+
+
+    /*======= Skillset *=======*/
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    
+    
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+    
+    
+  
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    new GitHubCalendar("#github-graph", "chiragthakkar1993");
+    
+    
+    /* Github Activity Feed - https://github.com/chiragthakkar1993/github-activity 
+    GitHubActivity.feed({
+		username: "chiragthakkar1993",
+		selector: "#feed",
+});
+
+*/
+
+});
